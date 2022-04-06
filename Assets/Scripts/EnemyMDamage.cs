bbeed8ccedf5ca1378d3bb57ebfdb74a280b55a8
Letyakunai <100812393+Letyakunai@users.noreply.github.com>
2022-04-06 11:37:21 +0800

    Minor Tweaks
    May hold some bugs.

--- Assets/Scripts/EnemyMDamage.cs
+++ Assets/Scripts/EnemyMDamage.cs
@@ -9,13 +9,27 @@ public class EnemyMDamage : MonoBehaviour
     public PlayerHP Playerhealth;
     public LevelLoader LeveltoLoad;
     //public Invulnerability Invul;
+
+    [Header("Invun Time")]
+    [SerializeField]
+    private float invunT;
+    private float interval;
+    
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
             PlayerScore.ScoreValue += 100;
             //Invul.StartCoroutine(IFrame());
-            Playerhealth.health -= 1;           
+
+            invunT += Time.deltaTime * 1;
+
+            if (invunT > interval)
+            {
+                Playerhealth.health -= 1;
+                invunT = 0.0f;
+            }
+            
         }
 
         if (Playerhealth.health <= 0)
